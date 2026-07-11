61eb07e92eb141332e0328bb46b2dc4860cc6690
João Pedro Silva Cunha <jpcunha5@gmail.com>
2026-07-11 00:21:44 -0300

    agora vai

--- Back-End/main.py
+++ Back-End/main.py
@@ -1,4 +1,5 @@
 ﻿from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
 from core.database import Base, motor
@@ -15,7 +16,7 @@ app = FastAPI(title="API Financeiro", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500","http://localhost:5500"],
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -34,3 +35,6 @@ Base.metadata.create_all(bind=motor)
 @app.get("/")
 def home():
     return {"mensagem": "API funcionando!"}
+
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
